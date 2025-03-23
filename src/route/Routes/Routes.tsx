847da7740dc8e0d88fcbe4cc05255627d9e901ff
Antonio Lucas <61833383+MisteriumEz@users.noreply.github.com>
2025-03-23 01:33:13 -0300

    feat(all): Ajustes no Index e Rotas

--- src/route/Routes/Routes.tsx
+++ src/route/Routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "../Protected/ProtectedRoute";
 import { HomePage, LandingPage, RegisterPage, LoginPage } from "../../pages";
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: "beyblade-big-site/",
+  base: "/beyblade-big-site/",
   build: {
     chunkSizeWarningLimit: 5000,
   },
