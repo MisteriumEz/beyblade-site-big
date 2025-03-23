8555c3421120f1bbd6f45c478d91d967eb1ef7b8
Antonio Lucas <61833383+MisteriumEz@users.noreply.github.com>
2025-03-23 02:08:11 -0300

    feat(src): Tentando deploy na vercell

--- src/route/Routes/Routes.tsx
+++ src/route/Routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "../Protected/ProtectedRoute";
 import { HomePage, LandingPage, RegisterPage, LoginPage } from "../../pages";
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: "/beyblade-big-site/",
   build: {
     chunkSizeWarningLimit: 5000,
   },
